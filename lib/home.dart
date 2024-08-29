22728b5a1cd7eefabe815e9dbef2ddeacda54330
KayesAshfaq <asfak7074@gmail.com>
2024-08-29 23:57:17 +0600

    feat: gridView updated with card collection

--- lib/home.dart
+++ lib/home.dart
@@ -17,7 +17,39 @@ import 'package:flutter/material.dart';
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
-  // TODO: Make a collection of cards
+  // Make a collection of cards
+  List<Card> _buildGridCards(int count) {
+    List<Card> cards = List.generate(
+      count,
+      (int index) {
+        return Card(
+          clipBehavior: Clip.antiAlias,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              AspectRatio(
+                aspectRatio: 18.0 / 11.0,
+                child: Image.asset('assets/diamond.png'),
+              ),
+              const Padding(
+                padding: EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 8.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text('Title'),
+                    SizedBox(height: 8.0),
+                    Text('Secondary Text'),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+    return cards;
+  }
+
   // TODO: Add a variable for Category
   @override
   Widget build(BuildContext context) {
@@ -60,13 +92,13 @@ class HomePage extends StatelessWidget {
           ),
         ],
       ),
-      // grid view
+      // Add a grid view
       body: GridView.count(
         crossAxisCount: 2,
         padding: const EdgeInsets.all(16.0),
         childAspectRatio: 8.0 / 9.0,
-        // TODO: Build a grid of cards (102)
-        children: <Widget>[Card()],
+        // Build a grid of cards
+        children: _buildGridCards(10),
       ),
 
       // TODO: Set resizeToAvoidBottomInset
