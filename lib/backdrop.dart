768d29c21da3cfbfeccade9a40f42403b0b8beaf
KayesAshfaq <asfak7074@gmail.com>
2024-08-30 02:00:45 +0600

    feat: custom appBar title added for animated backdrop trigger in backdrop.dart

--- lib/backdrop.dart
+++ lib/backdrop.dart
@@ -1,29 +1,10 @@
 import 'package:flutter/material.dart';
 
+import 'login.dart';
 import 'model/product.dart';
 
 // TODO: Add velocity constant
 const double _kFlingVelocity = 2.0;
-
-class Backdrop extends StatefulWidget {
-  final Category currentCategory;
-  final Widget frontLayer;
-  final Widget backLayer;
-  final Widget frontTitle;
-  final Widget backTitle;
-
-  const Backdrop({
-    required this.currentCategory,
-    required this.frontLayer,
-    required this.backLayer,
-    required this.frontTitle,
-    required this.backTitle,
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  _BackdropState createState() => _BackdropState();
-}
 
 // Add _FrontLayer class
 class _FrontLayer extends StatelessWidget {
@@ -63,6 +44,110 @@ class _FrontLayer extends StatelessWidget {
       ),
     );
   }
+}
+
+// Add _BackdropTitle class
+class _BackdropTitle extends AnimatedWidget {
+  final void Function() onPress;
+  final Widget frontTitle;
+  final Widget backTitle;
+
+  const _BackdropTitle({
+    Key? key,
+    required Animation<double> listenable,
+    required this.onPress,
+    required this.frontTitle,
+    required this.backTitle,
+  })  : _listenable = listenable,
+        super(key: key, listenable: listenable);
+
+  final Animation<double> _listenable;
+
+  @override
+  Widget build(BuildContext context) {
+    final Animation<double> animation = _listenable;
+
+    return DefaultTextStyle(
+      style: Theme.of(context).textTheme.titleLarge!,
+      softWrap: false,
+      overflow: TextOverflow.ellipsis,
+      child: Row(children: <Widget>[
+        // branded icon
+        SizedBox(
+          width: 72.0,
+          child: IconButton(
+            padding: const EdgeInsets.only(right: 8.0),
+            onPressed: this.onPress,
+            icon: Stack(children: <Widget>[
+              Opacity(
+                opacity: animation.value,
+                child: const ImageIcon(AssetImage('assets/slanted_menu.png')),
+              ),
+              FractionalTranslation(
+                translation: Tween<Offset>(
+                  begin: Offset.zero,
+                  end: const Offset(1.0, 0.0),
+                ).evaluate(animation),
+                child: const ImageIcon(AssetImage('assets/diamond.png')),
+              )
+            ]),
+          ),
+        ),
+        // Here, we do a custom cross fade between backTitle and frontTitle.
+        // This makes a smooth animation between the two texts.
+        Stack(
+          children: <Widget>[
+            Opacity(
+              opacity: CurvedAnimation(
+                parent: ReverseAnimation(animation),
+                curve: const Interval(0.5, 1.0),
+              ).value,
+              child: FractionalTranslation(
+                translation: Tween<Offset>(
+                  begin: Offset.zero,
+                  end: const Offset(0.5, 0.0),
+                ).evaluate(animation),
+                child: backTitle,
+              ),
+            ),
+            Opacity(
+              opacity: CurvedAnimation(
+                parent: animation,
+                curve: const Interval(0.5, 1.0),
+              ).value,
+              child: FractionalTranslation(
+                translation: Tween<Offset>(
+                  begin: const Offset(-0.25, 0.0),
+                  end: Offset.zero,
+                ).evaluate(animation),
+                child: frontTitle,
+              ),
+            ),
+          ],
+        )
+      ]),
+    );
+  }
+}
+
+class Backdrop extends StatefulWidget {
+  final Category currentCategory;
+  final Widget frontLayer;
+  final Widget backLayer;
+  final Widget frontTitle;
+  final Widget backTitle;
+
+  const Backdrop({
+    required this.currentCategory,
+    required this.frontLayer,
+    required this.backLayer,
+    required this.frontTitle,
+    required this.backTitle,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  _BackdropState createState() => _BackdropState();
 }
 
 // Add _BackdropTitle class
@@ -147,26 +232,25 @@ class _BackdropState extends State<Backdrop> with SingleTickerProviderStateMixin
     var appBar = AppBar(
       elevation: 0.0,
       titleSpacing: 0.0,
-      // TODO: Replace leading menu icon with IconButton
-      // TODO: Remove leading property
-
-      // Create title with _BackdropTitle parameter
-      // Replace leading menu icon with IconButton
-      leading: IconButton(
-        icon: const Icon(Icons.menu),
-        onPressed: _toggleBackdropLayerVisibility,
+      title: _BackdropTitle(
+        listenable: _controller.view,
+        onPress: _toggleBackdropLayerVisibility,
+        frontTitle: widget.frontTitle,
+        backTitle: widget.backTitle,
       ),
-
-      title: Text('SHRINE'),
       actions: <Widget>[
         // TODO: Add shortcut to login screen from trailing icons
         IconButton(
-          icon: Icon(
+          icon: const Icon(
             Icons.search,
             semanticLabel: 'search',
           ),
           onPressed: () {
-            // TODO: Add open login
+            // Add open login
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (BuildContext context) => const LoginPage()),
+            );
           },
         ),
         IconButton(
@@ -175,7 +259,11 @@ class _BackdropState extends State<Backdrop> with SingleTickerProviderStateMixin
             semanticLabel: 'filter',
           ),
           onPressed: () {
-            // TODO: Add open login
+            // Add open login
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (BuildContext context) => const LoginPage()),
+            );
           },
         ),
       ],
